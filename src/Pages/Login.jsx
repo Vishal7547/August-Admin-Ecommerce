efba90616f29da29c932270d5c43da4dc8b7c53c
Mansi523 <kumarimansi523@gmail.com>
2023-12-27 18:29:44 -0800

    designed login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
-
+import style from "../Pages/Login.module.css";
 const Login = () => {
   return (
-    <div>
-      <h1>login</h1>
+   <div className={style.formcontainer}>
+    <div className={style.formouter}>
+ 
+     <div className={style.sideimage}>
+
+   
+     </div>
+      <form className={style.sigin}>
+     <div className={style.input}>
+       <div className={style.emailHeader}>
+         <span>*Log in using Email</span>
+       </div>
+       <input
+         value=""
+         className={style.emailInput}
+         type='text'
+         placeholder='Enter Email'
+        //  onChange={(e) => setEmail(e.target.value)}
+       />
+     </div>
+     <div className="otp">
+       <div className={style.password}>
+         <span>*Enter Password</span>
+       </div>
+       <input
+         value=""
+         className={style.passwordInput}
+         type='password'
+         placeholder='Enter Password'
+        //  onChange={(e) => setPassword(e.target.value)}
+         autoComplete='off'
+       />
+     </div>
+     <button className={style.btnlogin}>
+       Login
+     </button>
+     </form>
+
     </div> 
+    </div>
   )
 }
 
